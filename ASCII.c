3163fcdfb11eb4322ceb04286c608d2face20fc2
dj-messi <72274379+dj-messi@users.noreply.github.com>
2020-10-02 13:54:22 -0400

    Ascii

---
+++ ASCII.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i;
+    char ch[101];
+    gets(ch);
+    int n=strlen(ch);
+    printf("ASCII values of characters are:");
+    for( i=0; i<n; i++)
+    {
+        printf("%c--%d\n",ch[i], ch[i]);
+    }
+    return 0;
+}
