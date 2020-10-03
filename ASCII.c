52bdc1a6edfae7d387d986d65f868c6bf95fc75e
Harshtiwari12 <55392582+Harshtiwari12@users.noreply.github.com>
2020-10-03 21:53:25 +0530

    update ASCII.c
    ..

--- ASCII.c
+++ ASCII.c
@@ -6,7 +6,7 @@ int main()
     char ch[101];
     gets(ch);
     int n=strlen(ch);
-    printf("ASCII values of characters are:");
+    printf("ASCII values of characters are:\n\n");
     for( i=0; i<n; i++)
     {
         printf("%c--%d\n",ch[i], ch[i]);
